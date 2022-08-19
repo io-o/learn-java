8a43561bb948ab656f56ea48359a50d197c09600
io_o <>
2022-08-19 22:18:49 +0800

    feat: add GlobalExceptionHandle

--- src/main/java/com/example/demo/common/GlobalExceptionHandle.java
+++ src/main/java/com/example/demo/common/GlobalExceptionHandle.java
@@ -1,4 +1,32 @@
 package com.example.demo.common;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+
+// 拦截controller
+@ControllerAdvice(annotations = {RestController.class})
+@ResponseBody
+@Slf4j
 public class GlobalExceptionHandle {
+    // 拦截异常的错误类型
+    // 处理SQL的异常处理
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public R<String> exceptionHandle(SQLIntegrityConstraintViolationException ex) {
+        log.error(ex.getMessage());
+
+        if (ex.getMessage().contains("Duplicate entry")) {
+            String[] errArr = ex.getMessage().split(" ");
+
+            String msg = errArr[2] + "已存在";
+
+            return R.error(msg);
+        }
+
+        return R.error("未知错误");
+    }
 }
